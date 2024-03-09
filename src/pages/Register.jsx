30c98e7c8995dd1d42110d7f910f8c37403aa9e0
Joseph Ruiz <josephruiz.molina@gmail.com>
2024-03-08 21:30:08 -0400

    idk

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import { createUser, registerClick } from "../controllers/users";
@@ -58,4 +59,34 @@ export default function Register() {
       </section>
     </div>
   );
+=======
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function Register() {
+    return (
+        <div>
+            <section>
+                <h1>Registrarse</h1>
+                <section>
+                    <h2>Correo</h2>
+                    <input type="text" placeholder="Ej: jose.hurtado@gmail.com"></input>
+                </section>
+                <section>
+                    <h2>Usuario</h2>
+                    <input type="text" placeholder="Ej: Phox01"></input>
+                </section>
+                <section>
+                    <h2>Contraseña</h2>
+                    <input type="password"></input>
+                </section>
+                <section>
+                    <button>Registrarse</button>
+                    <button>Registrarse con Google</button>
+                </section>
+                <Link to="/">¿Ya tienes una cuenta? Inicia Sesión</Link>
+            </section>
+        </div>
+    )
+>>>>>>> parent of b9a63d9 (Firebase y Registro)
 }
