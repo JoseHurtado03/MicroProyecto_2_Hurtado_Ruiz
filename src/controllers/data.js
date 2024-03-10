c055b62cf2692f0fb9a02e5c91af387fcb84d2b6
Joseph Ruiz <josephruiz.molina@gmail.com>
2024-03-10 17:36:58 -0400

    listo el error 😎

--- src/controllers/data.js
+++ src/controllers/data.js
@@ -1,34 +1,34 @@
-import { collection, getDocs, getDoc } from "firebase/firestore";
+import { collection, getDocs, getDoc, doc } from "firebase/firestore";
 import { db } from "../firebase";
 
-export async function getClubs(){
-    const clubsCollections = collection(db, 'Clubes')
-    const clubDocs = await getDocs(clubsCollections)
-    const clubs = clubDocs.docs.map((doc) => doc.data())
-    console.log(clubs)
-    return clubs
+export async function getClubs() {
+  const clubsCollections = collection(db, "Clubes");
+  const clubDocs = await getDocs(clubsCollections);
+  const clubs = clubDocs.docs.map((doc) => doc.data());
+  console.log(clubs);
+  return clubs;
 }
 
-export async function getClubsByID(id){
-    const clubsCollections = collection(db, 'Clubes');
-    const clubDoc = await getDoc(clubsCollections, id);
-    const club = clubDoc.data();
-    console.log(club)
-    return club
+export async function getClubsByID(id) {
+  const clubsCollection = collection(db, "Clubes");
+  const clubDocRef = doc(clubsCollection, id); // Create a reference to the specific document
+  const clubDoc = await getDoc(clubDocRef); // Pass the document reference to getDoc
+  const club = clubDoc.data();
+  return club;
 }
 
-export async function getGames(){
-    const gamesCollections = collection(db, 'Videojuegos')
-    const gameDocs = await getDocs(gamesCollections);
-    const games = gameDocs.docs.map((doc) => doc.data())
-    console.log(games)
-    return games
+export async function getGames() {
+  const gamesCollections = collection(db, "Videojuegos");
+  const gameDocs = await getDocs(gamesCollections);
+  const games = gameDocs.docs.map((doc) => doc.data());
+  console.log(games);
+  return games;
 }
 
-export async function getGamesByID(){
-    const gamesCollections = collection(db, 'Videojuegos')
-    const gameDocs = await getDocs(gamesCollections, id);
-    const game = gameDocs.data()
-    console.log(game)
-    return game
+export async function getGamesByID() {
+  const gamesCollections = collection(db, "Videojuegos");
+  const gameDocs = await getDocs(gamesCollections, id);
+  const game = gameDocs.data();
+  console.log(game);
+  return game;
 }
--- src/pages/Club.jsx
+++ src/pages/Club.jsx
@@ -1,36 +1,37 @@
-import React from 'react'
-import GameCard from './GameCard'
-import { getClubsByID } from '../controllers/data'
-import { useState, useEffect } from 'react'
+import React from "react";
+import GameCard from "./GameCard";
+import { getClubsByID } from "../controllers/data";
+import { useState, useEffect } from "react";
 
 export default function Club() {
-    const [clubInfo, setClubInfo] = useState(null);
-    useEffect(() => {
-        const selectedClubId = localStorage.getItem('ClubId');
-        const fetchClubInfo = async () => {
-            try {
-                const clubData = await getClubsByID(selectedClubId);
-                setClubInfo(clubData);
-            } catch (error) {
-                console.error('Error al obtener la información del club:', error);
-            }
-        };
+  const [clubInfo, setClubInfo] = useState("");
+  useEffect(() => {
+    const selectedClubId = localStorage.getItem("ClubId");
+    console.log(selectedClubId);
+    const fetchClubInfo = async () => {
+      try {
+        const clubData = await getClubsByID(selectedClubId);
+        setClubInfo(clubData);
+      } catch (error) {
+        console.error("Error al obtener la información del club:", error);
+      }
+    };
 
-        fetchClubInfo();
-    }, []);
+    fetchClubInfo();
+  }, []);
 
-    return (
-        <div>
-            <section>
-                <h1>{clubInfo.nombre}</h1>
-                <button>Buscar</button>
-            </section>
-            <section>{clubInfo.descripcion}</section>
-            <section>
-                <GameCard/>
-                <GameCard/>
-                <GameCard/>
-            </section>
-        </div>
-    )
+  return (
+    <div>
+      <section>
+        <h1>{clubInfo.nombre}</h1>
+        <button>Buscar</button>
+      </section>
+      <section>{clubInfo.descripcion}</section>
+      <section>
+        <GameCard />
+        <GameCard />
+        <GameCard />
+      </section>
+    </div>
+  );
 }
