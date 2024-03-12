1c1bedd05bbfc2143c7fb3f930150214fa0ef0da
Joseph Ruiz <josephruiz.molina@gmail.com>
2024-03-11 23:17:06 -0400

    :zap: final
    end...?

--- src/controllers/data.js
+++ src/controllers/data.js
@@ -1,54 +1,74 @@
-import { collection, getDocs, getDoc, doc } from "firebase/firestore";
+import {
+  collection,
+  getDocs,
+  getDoc,
+  doc,
+  updateDoc,
+} from "firebase/firestore";
 import { db } from "../firebase";
 
 export async function getClubs() {
-    const clubsCollections = collection(db, "Clubes");
-    const clubDocs = await getDocs(clubsCollections);
-    const clubs = clubDocs.docs.map((doc) => doc.data());
-    console.log(clubs);
-    return clubs;
+  const clubsCollections = collection(db, "Clubes");
+  const clubDocs = await getDocs(clubsCollections);
+  const clubs = clubDocs.docs.map((doc) => doc.data());
+  console.log(clubs);
+  return clubs;
+}
+
+export async function updateProfile(
+  id,
+  { email, lastName, name, pref_game, username }
+) {
+  const usersCollections = collection(db, "users");
+  await updateDoc(doc(usersCollections, id), {
+    email,
+    lastName,
+    name,
+    pref_game,
+    username,
+  });
 }
 
 export async function getClubsByID(id) {
-    const clubsCollection = collection(db, "Clubes");
-    const clubDocRef = doc(clubsCollection, id); // Create a reference to the specific document
-    const clubDoc = await getDoc(clubDocRef); // Pass the document reference to getDoc
-    const club = clubDoc.data();
-    return club;
+  const clubsCollection = collection(db, "Clubes");
+  const clubDocRef = doc(clubsCollection, id); // Create a reference to the specific document
+  const clubDoc = await getDoc(clubDocRef); // Pass the document reference to getDoc
+  const club = clubDoc.data();
+  return club;
 }
 
 export async function getGames() {
-    const gamesCollections = collection(db, "Videojuegos");
-    const gameDocs = await getDocs(gamesCollections);
-    const games = gameDocs.docs.map((doc) => doc.data());
-    console.log(games);
-    return games;
+  const gamesCollections = collection(db, "Videojuegos");
+  const gameDocs = await getDocs(gamesCollections);
+  const games = gameDocs.docs.map((doc) => doc.data());
+  console.log(games);
+  return games;
 }
 
 export async function getGamesByID(id) {
-    const gamesCollections = collection(db, "Videojuegos");
-    const gameDocRef = doc(gamesCollections, id);
-    const gameDoc = await getDoc(gameDocRef);
-    const game = gameDoc.data();
-    console.log(game);
-    return game;
+  const gamesCollections = collection(db, "Videojuegos");
+  const gameDocRef = doc(gamesCollections, id);
+  const gameDoc = await getDoc(gameDocRef);
+  const game = gameDoc.data();
+  console.log(game);
+  return game;
 }
 
 export async function getGamesByGenre(genre) {
-    const gamesCollections = collection(db, "Videojuegos");
-    const gameDocs = await getDocs(gamesCollections);
-    const games = gameDocs.docs
-      .map((doc) => doc.data())
-      .filter((game) => game.genero.toLowerCase() === genre.toLowerCase());
-    console.log(games);
-    return games;
+  const gamesCollections = collection(db, "Videojuegos");
+  const gameDocs = await getDocs(gamesCollections);
+  const games = gameDocs.docs
+    .map((doc) => doc.data())
+    .filter((game) => game.genero.toLowerCase() === genre.toLowerCase());
+  console.log(games);
+  return games;
 }
 
-export async function getUserByID(id){
-    const usersCollections = collection(db, "users");
-    const userDocRef = doc(usersCollections, id);
-    const userDoc = await getDoc(userDocRef);
-    const user = userDoc.data();
-    console.log(user);
-    return user;
+export async function getUserByID(id) {
+  const usersCollections = collection(db, "users");
+  const userDocRef = doc(usersCollections, id);
+  const userDoc = await getDoc(userDocRef);
+  const user = userDoc.data();
+  console.log(user);
+  return user;
 }
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -5,6 +5,7 @@ import { logOut } from "../controllers/auth";
 import { Link } from "react-router-dom";
 import { useUser } from "../context/user";
 import { useNavigate } from "react-router-dom";
+import { updateProfile } from "../controllers/data";
 
 import { getUserByID } from "../controllers/data";
 import { onAuthStateChanged } from "firebase/auth";
@@ -12,20 +13,42 @@ import { auth } from "../firebase";
 
 export default function Profile() {
   const [user, setUser] = useState(null);
-
+  const [email, setEmail] = useState(""); //NUEVO NUEVO
+  const [name, setName] = useState(""); //NUEVO NUEVO
+  const [lastName, setLastName] = useState(""); //NUEVO NUEVO
+  const [username, setUsername] = useState(""); //NUEVO NUEVO
+  const [prefGame, setPrefGame] = useState("");
   const getUserInfo = async () => {
     try {
       const userID = localStorage.getItem("userID");
-      console.log("Hi, here begins test");
-      console.log(userID);
       const userData = await getUserByID(userID);
-      console.log(userData);
       setUser(userData);
-      console.log(user);
+      setEmail(userData.email);
+      setName(userData.name);
+      setLastName(userData.lastName);
+      setUsername(userData.username);
+      setPrefGame(userData.pref_game);
     } catch (error) {
       console.error("Error al obtener la información del usuario:", error);
     }
   };
+
+  const handleUpdateProfile = async () => {
+    try {
+      const userID = localStorage.getItem("userID");
+      await updateProfile(userID, {
+        email,
+        name,
+        lastName,
+        username,
+        pref_game: prefGame,
+      });
+      alert("Perfil actualizado correctamente");
+    } catch (error) {
+      console.error("Error al actualizar el perfil:", error);
+    }
+  };
+  //NUEVO NUEVO NUEVO NUEVO NUEVO NUEVO NUEVO
 
   useEffect(() => {
     getUserInfo();
@@ -60,125 +83,41 @@ export default function Profile() {
             <div className={styles.text}>Correo</div>
             <input
               placeholder={user ? user.email : ""}
+              value={email} //NUEVO NUEVO
+              onChange={(e) => setEmail(e.target.value)} //NUEVO NUEVO
               className={styles.input}
             ></input>
-            <button>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="40"
-                height="40"
-                viewBox="0 0 40 40"
-                fill="none"
-              >
-                <path
-                  d="M11.6665 11.6667H9.99984C9.11578 11.6667 8.26794 12.0179 7.64281 12.643C7.01769 13.2681 6.6665 14.116 6.6665 15V30C6.6665 30.8841 7.01769 31.7319 7.64281 32.357C8.26794 32.9821 9.11578 33.3333 9.99984 33.3333H24.9998C25.8839 33.3333 26.7317 32.9821 27.3569 32.357C27.982 31.7319 28.3332 30.8841 28.3332 30V28.3333"
-                  stroke="black"
-                  stroke-width="3.33333"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-                <path
-                  d="M26.6667 8.33334L31.6667 13.3333M33.975 10.975C34.6314 10.3186 35.0002 9.42831 35.0002 8.5C35.0002 7.5717 34.6314 6.68141 33.975 6.025C33.3186 5.36859 32.4283 4.99982 31.5 4.99982C30.5717 4.99982 29.6814 5.36859 29.025 6.025L15 20V25H20L33.975 10.975Z"
-                  stroke="black"
-                  stroke-width="3.33333"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-              </svg>
-            </button>
+            <button onClick={handleUpdateProfile}>Actualizar</button>
           </main>
           <main className={styles.field}>
             <div className={styles.text}>Nombre</div>
             <input
               placeholder={user ? user.name : ""}
+              value={name} //NUEVO NUEVO
+              onChange={(e) => setName(e.target.value)} //NUEVO NUEVO
               className={styles.input}
             ></input>
-            <button>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="40"
-                height="40"
-                viewBox="0 0 40 40"
-                fill="none"
-              >
-                <path
-                  d="M11.6665 11.6667H9.99984C9.11578 11.6667 8.26794 12.0179 7.64281 12.643C7.01769 13.2681 6.6665 14.116 6.6665 15V30C6.6665 30.8841 7.01769 31.7319 7.64281 32.357C8.26794 32.9821 9.11578 33.3333 9.99984 33.3333H24.9998C25.8839 33.3333 26.7317 32.9821 27.3569 32.357C27.982 31.7319 28.3332 30.8841 28.3332 30V28.3333"
-                  stroke="black"
-                  stroke-width="3.33333"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-                <path
-                  d="M26.6667 8.33334L31.6667 13.3333M33.975 10.975C34.6314 10.3186 35.0002 9.42831 35.0002 8.5C35.0002 7.5717 34.6314 6.68141 33.975 6.025C33.3186 5.36859 32.4283 4.99982 31.5 4.99982C30.5717 4.99982 29.6814 5.36859 29.025 6.025L15 20V25H20L33.975 10.975Z"
-                  stroke="black"
-                  stroke-width="3.33333"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-              </svg>
-            </button>
+            <button onClick={handleUpdateProfile}>Actualizar</button>
           </main>
           <main className={styles.field}>
             <div className={styles.text}>Apellido</div>
             <input
               placeholder={user ? user.lastName : ""}
+              value={lastName} //NUEVO NUEVO
+              onChange={(e) => setLastName(e.target.value)} //NUEVO NUEVO
               className={styles.input}
             ></input>
-            <button>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="40"
-                height="40"
-                viewBox="0 0 40 40"
-                fill="none"
-              >
-                <path
-                  d="M11.6665 11.6667H9.99984C9.11578 11.6667 8.26794 12.0179 7.64281 12.643C7.01769 13.2681 6.6665 14.116 6.6665 15V30C6.6665 30.8841 7.01769 31.7319 7.64281 32.357C8.26794 32.9821 9.11578 33.3333 9.99984 33.3333H24.9998C25.8839 33.3333 26.7317 32.9821 27.3569 32.357C27.982 31.7319 28.3332 30.8841 28.3332 30V28.3333"
-                  stroke="black"
-                  stroke-width="3.33333"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-                <path
-                  d="M26.6667 8.33334L31.6667 13.3333M33.975 10.975C34.6314 10.3186 35.0002 9.42831 35.0002 8.5C35.0002 7.5717 34.6314 6.68141 33.975 6.025C33.3186 5.36859 32.4283 4.99982 31.5 4.99982C30.5717 4.99982 29.6814 5.36859 29.025 6.025L15 20V25H20L33.975 10.975Z"
-                  stroke="black"
-                  stroke-width="3.33333"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-              </svg>
-            </button>
+            <button onClick={handleUpdateProfile}>Actualizar</button>
           </main>
           <main className={styles.field}>
             <div className={styles.text}>Usuario</div>
             <input
               placeholder={user ? user.username : ""}
+              value={username} //NUEVO NUEVO
+              onChange={(e) => setUsername(e.target.value)} //NUEVO NUEVO
               className={styles.input}
             ></input>
-            <button>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="40"
-                height="40"
-                viewBox="0 0 40 40"
-                fill="none"
-              >
-                <path
-                  d="M11.6665 11.6667H9.99984C9.11578 11.6667 8.26794 12.0179 7.64281 12.643C7.01769 13.2681 6.6665 14.116 6.6665 15V30C6.6665 30.8841 7.01769 31.7319 7.64281 32.357C8.26794 32.9821 9.11578 33.3333 9.99984 33.3333H24.9998C25.8839 33.3333 26.7317 32.9821 27.3569 32.357C27.982 31.7319 28.3332 30.8841 28.3332 30V28.3333"
-                  stroke="black"
-                  stroke-width="3.33333"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-                <path
-                  d="M26.6667 8.33334L31.6667 13.3333M33.975 10.975C34.6314 10.3186 35.0002 9.42831 35.0002 8.5C35.0002 7.5717 34.6314 6.68141 33.975 6.025C33.3186 5.36859 32.4283 4.99982 31.5 4.99982C30.5717 4.99982 29.6814 5.36859 29.025 6.025L15 20V25H20L33.975 10.975Z"
-                  stroke="black"
-                  stroke-width="3.33333"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-              </svg>
-            </button>
+            <button onClick={handleUpdateProfile}> Actualizar</button>
           </main>
           <main className={styles.field}>
             <div className={styles.text} id={styles["vgame"]}>
@@ -186,67 +125,23 @@ export default function Profile() {
             </div>
             <input
               placeholder={user ? user.pref_game : ""}
+              value={prefGame} //NUEVO NUEVO
+              onChange={(e) => setPrefGame(e.target.value)} //NUEVO NUEVO
               className={styles.input}
             ></input>
-            <button>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="40"
-                height="40"
-                viewBox="0 0 40 40"
-                fill="none"
-              >
-                <path
-                  d="M11.6665 11.6667H9.99984C9.11578 11.6667 8.26794 12.0179 7.64281 12.643C7.01769 13.2681 6.6665 14.116 6.6665 15V30C6.6665 30.8841 7.01769 31.7319 7.64281 32.357C8.26794 32.9821 9.11578 33.3333 9.99984 33.3333H24.9998C25.8839 33.3333 26.7317 32.9821 27.3569 32.357C27.982 31.7319 28.3332 30.8841 28.3332 30V28.3333"
-                  stroke="black"
-                  stroke-width="3.33333"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-                <path
-                  d="M26.6667 8.33334L31.6667 13.3333M33.975 10.975C34.6314 10.3186 35.0002 9.42831 35.0002 8.5C35.0002 7.5717 34.6314 6.68141 33.975 6.025C33.3186 5.36859 32.4283 4.99982 31.5 4.99982C30.5717 4.99982 29.6814 5.36859 29.025 6.025L15 20V25H20L33.975 10.975Z"
-                  stroke="black"
-                  stroke-width="3.33333"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-              </svg>
-            </button>
+            <button onClick={handleUpdateProfile}> Actualizar</button>
           </main>
           <main className={styles.field}>
             <div className={styles.text}>Contraseña</div>
             <input placeholder="********" className={styles.input}></input>
-            <button>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="40"
-                height="40"
-                viewBox="0 0 40 40"
-                fill="none"
-              >
-                <path
-                  d="M11.6665 11.6667H9.99984C9.11578 11.6667 8.26794 12.0179 7.64281 12.643C7.01769 13.2681 6.6665 14.116 6.6665 15V30C6.6665 30.8841 7.01769 31.7319 7.64281 32.357C8.26794 32.9821 9.11578 33.3333 9.99984 33.3333H24.9998C25.8839 33.3333 26.7317 32.9821 27.3569 32.357C27.982 31.7319 28.3332 30.8841 28.3332 30V28.3333"
-                  stroke="black"
-                  stroke-width="3.33333"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-                <path
-                  d="M26.6667 8.33334L31.6667 13.3333M33.975 10.975C34.6314 10.3186 35.0002 9.42831 35.0002 8.5C35.0002 7.5717 34.6314 6.68141 33.975 6.025C33.3186 5.36859 32.4283 4.99982 31.5 4.99982C30.5717 4.99982 29.6814 5.36859 29.025 6.025L15 20V25H20L33.975 10.975Z"
-                  stroke="black"
-                  stroke-width="3.33333"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-              </svg>
-            </button>
+            <button>Actualizar</button>
           </main>
         </article>
       </section>
       <section>
-        <button onClick={logOut} className={styles.logout}>
+        <Link to="/" onClick={logOut} className={styles.logout}>
           Cerrar Sesión
-        </button>
+        </Link>
         <article
           className={styles.box}
           style={{ width: "1px" }}
